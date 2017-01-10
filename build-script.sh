f223bd28348de7ca45285f5d9f92cc9a5fccbee0
Alexander Kmoch <allixender@googlemail.com>
2017-01-10 16:37:52 +1300

    maybe patch fortran demo? service.for best typo

--- build-script.sh
+++ build-script.sh
@@ -101,7 +101,7 @@ cd $ZOO_BUILD_DIR/zoo-services/hello-py/ \
   && cp cgi-env/*.* $CGI_DIR/ || exit 1
 
 cd $ZOO_BUILD_DIR/zoo-services/hello-fortran/ \
-  && ln service.f service.for \
+  && ln service.f servive.for \
   && make \
   && cp cgi-env/*.* $CGI_DIR/ || exit 1
 
